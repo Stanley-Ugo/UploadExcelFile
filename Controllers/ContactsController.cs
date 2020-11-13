ac167451f69d31ac3e96ee16e8553a3b7b4f49ea
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-11-13 23:21:58 +0100

    Initial commit - initialized project

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+using UploadExcelFile.Models;
+
+namespace UploadExcelFile.Controllers
+{
+    public class ContactsController : Controller
+    {
+        // GET: Contacts
+        [HttpGet]
+        public ActionResult Index()
+        {
+            
+        }
+
+        //POST: Contacts
+        [HttpPost]
+        public ActionResult Index()
+        {
+
+        }
+    }
+}
