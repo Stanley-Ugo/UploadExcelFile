0f99a891d406211ea88420c2aa18e5e024c18843
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-11-14 14:08:56 +0100

    handling exceptions - try{}catch{}

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -27,42 +27,77 @@ namespace UploadExcelFile.Controllers
             string filePath = string.Empty;
             if (postedFile != null)
             {
-                string path = Server.MapPath("~/Uploads/");
-                if (!Directory.Exists(path))
+                try
                 {
-                    Directory.CreateDirectory(path);
-                }
-                filePath = path + Path.GetFileName(postedFile.FileName);
-                string extension = Path.GetExtension(postedFile.FileName);
-                postedFile.SaveAs(filePath);
-
-                //Read the contents of CSV file.
-                string csvData = System.IO.File.ReadAllText(filePath);
-
-                //Execute a loop over the rows.
-                foreach (string row in csvData.Split('\n'))
-                {
-                    if (!string.IsNullOrEmpty(row))
+                    string path = Server.MapPath("~/Uploads/");
+                    if (!Directory.Exists(path))
                     {
+                        Directory.CreateDirectory(path);
+                    }
+                    filePath = path + Path.GetFileName(postedFile.FileName);
+                    string extension = Path.GetExtension(postedFile.FileName);
+                    postedFile.SaveAs(filePath);
 
-                        contact.Add(new Contact
+                    //Read the contents of CSV file.
+                    string csvData = System.IO.File.ReadAllText(filePath);
+
+                    //Execute a loop over the rows.
+                    foreach (string row in csvData.Split('\n'))
+                    {
+                        if (!string.IsNullOrEmpty(row))
                         {
-                            FirstName = row.Split(',')[0], //Convert.ToInt32(row.Split(',')[0]),
-                            LastName = row.Split(',')[1],
-                            Email = row.Split(',')[2],
-                            Telephone = row.Split(',')[3],
-                            Mobile = row.Split(',')[4],
-                            CompanyID = Convert.ToInt32(row.Split(',')[5])
-                        });
+                            string firstName = row.Split(',')[0];
+                            string lastName = row.Split(',')[1];
+                            string email = row.Split(',')[2];
+                            string telephone = row.Split(',')[3];
+                            string mobile = row.Split(',')[4];
+                            int companyId = Convert.ToInt32(row.Split(',')[5]);
 
-                        this.PostToDatabase(contact);
+                            //Checking then first Name field
+                            if (firstName == string.Empty)
+                                TempData["Message"] = "First Name Field can not be Empty";
+
+                            //checking the Last Name field
+                            if (lastName == string.Empty)
+                                TempData["Message"] = "Last Name Field can not be Empty";
+                            
+
+                            //checking for email field
+                            if (email == string.Empty)
+                                TempData["Message"] = "Email Field can not be Empty";
+
+                            //checking for Mobile
+                            if (mobile == string.Empty)
+                                TempData["Message"] = "Mobile Field can not be Empty";
+
+                            //checking for Valid Company Id
+                            if (companyId != 7)
+                                TempData["Message"] = "Invalid COmpany ID";
+
+
+                            contact.Add(new Contact
+                            {
+                                FirstName = firstName,
+                                LastName = lastName,
+                                Email = email,
+                                Telephone = telephone,
+                                Mobile = mobile,
+                                CompanyID = companyId
+
+
+                            });
+
+                            this.PostToDatabase(contact);
+                        }
                     }
                 }
+                catch (Exception ex)
+                {
+                    TempData["Message"] = "No File Chosen";
+                    TempData["Message"] = "Something went wrong " + ex.Message;
+                }
             }
-            else
-            {
-                TempData["Message"] = "No File Chosen";
-            }
+           
 
             return View(contact);
         }
@@ -70,7 +105,7 @@ namespace UploadExcelFile.Controllers
         private void PostToDatabase(List<Contact> contact)
         {
             //ADO>NET CODE connection string
-            TempData["Message"] = "No File Chosen";
+            
 
         }
     }
