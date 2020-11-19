a978e5db176677efbf378681454b83752c587da2
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-11-20 00:20:38 +0100

    Adding the ContactBatch Controller

---
+++ Controllers/ContactBatchController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UploadExcelFile.Controllers
+{
+    public class ContactBatchController : Controller
+    {
+        // GET: ContactBatch
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
